7e0341390cb1a5f043a8071fc3b57176d52dbda9
OYD03 <oumar.yacoubadiawara@gmail.com>
2022-11-28 17:13:29 +0100

    troisieme commit

--- oyd/index.php
+++ oyd/index.php
@@ -8,13 +8,16 @@
 	<link rel="stylesheet" href="./css/header.css">
 	<link rel="stylesheet" href="./css/footer.css">
 	<link rel="stylesheet" href="./css/vertical-navbar.css">
+	<link rel="stylesheet" href="./css/content.css">
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,500,600,700&amp;display=swap" rel="stylesheet">
+	<script src="./js/script.js" defer></script>
 	<title>GREI</title>
 </head>
 <body>
 	<header>
 		<div class="container">
-			<div class="burger">
-				<button class="burger"><span class="bar">-</span></button>
+			<div class="burger-bar">
+				<button class="burger" id="burger"><span class="bar"></span></button>
 			</div>
 			<div class="buttons">
 				<div class="user_icon">
@@ -33,31 +36,72 @@
 	<section class="vertical-navbar">
 		<div class="container">
 			<ul class="menu unstyled-list">
-				<li><a href="">Tableau de bord</a></li>
-				<li><a href="">Messagerie</a></li>
-				<li><a href="">Gestion de données</a></li>
-				<li><a href="">Compte</a></li>
-				<li><a href="">Deconnexion</a></li>
+				<?php require_once("./vues/navbar/nav-admin.php"); ?>
 			</ul>
 		</div>
+	</section>
+	<section class="content">
+	<?php
+		if(isset($_GET['user'])){
+			$user = $_GET['user'];
+			switch ($user) {
+				case 'amdin':
+					if(isset($_GET['page'])){
+						$page = $_GET['page'];
+						switch ($page) {
+							case 0:
+								require_once("./vues/tableau_de_bord/tdb-admin.php");
+								break;
+							// case 1:
+							// 	require_once("./vues/tableau_de_bord/tdb-admin.php");
+							// 	break;
+							// case 2:
+							// 	require_once("./vues/tableau_de_bord/tdb-admin.php");
+							// 	break;
+							// case 3:
+							// 	require_once("./vues/tableau_de_bord/tdb-admin.php");
+							// 	break;
+							// case 4:
+							// 	require_once("./vues/tableau_de_bord/tdb-admin.php");
+							// 	break;
+							
+							default:
+								require_once("./vues/tableau_de_bord/tdb-admin.php");
+								break;
+						}
+					}
+					break;
+				// case 'prof':
+				// 	# code...
+				// 	break;
+				// case 'etudiant':
+				// 	# code...
+				// 	break;
+				
+				default:
+					require_once("./vues/tableau_de_bord/tdb-admin.php");
+					break;
+			}
+		}
+	?>
 	</section>
 	<footer>
 		<div class="container">
 			<div class="contacts">
 				<div class="phone">
 					<img src="./img/icons colorées/telephone.png" width="60" height="60">
-					<p>+33(0)144018670</p>
+					<p class="diff-font">+33(0)144018670</p>
 				</div>
 				<div class="mail">
 					<a href="mailto:contact@ecoleiris.fr">
 						<img src="./img/icons colorées/email.png" width="60" height="60">
-						<p>contact@ecoleiris.fr</p>
+						<p class="diff-font">contact@ecoleiris.fr</p>
 					</a>
 				</div>
 				<div class="link">
 					<a href="https://ecoleiris.fr/" target="_blank">
 						<img src="./img/icons colorées/lien.png" width="60" height="60">
-						<p>https://ecoleiris.fr/</p>
+						<p class="diff-font">https://ecoleiris.fr/</p>
 					</a>
 				</div>
 			</div>
---
+++ oyd/vues/tableau_de_bord/tdb-admin.php
@@ -0,0 +1,15 @@
+<h1>Tableau de bord admin</h1>
+<section class="subnavbar">
+  <div class="container">
+    <div class="submenu">
+      <ul class="submenu-list unstyled-list">
+        <li><a href="http://" target="_blank">Info Trafic</a></li>
+        <li><a href="http://" target="_blank">Ticket de retard</a></li>
+      </ul>
+    </div>
+    <div class="search-bar">
+      <input type="text" id="recherche" name="recherche" valuer="recherche">
+      <input type="submit" name="Rechercher" value="Rechercher" class="button">
+    </div>
+  </div>
+</section>
