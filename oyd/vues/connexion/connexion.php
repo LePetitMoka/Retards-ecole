07848117a98e3c14f1c7a43a23a6329585594552
OYD03 <oumar.yacoubadiawara@gmail.com>
2022-12-01 18:15:27 +0100

    commit

---
+++ oyd/vues/connexion/connexion.php
@@ -0,0 +1,16 @@
+<section class="role-choice">
+  <div class="container">
+    <div class="role-admin">
+      <img src="./img/icons colorées/administrateur.png" width="300" height="300">
+      <p>Administrateur</p>
+    </div>
+    <div class="role-prof">
+      <img src="./img/icons colorées/professeur.png" width="300" height="300">
+      <p>Professeur</p>
+    </div>
+    <div class="role-etudiant">
+      <img src="./img/icons colorées/etudiant.png" width="300" height="300">
+      <p>Etudiant</p>
+    </div>
+  </div>
+</section>
