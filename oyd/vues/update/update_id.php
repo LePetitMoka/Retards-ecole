7dcb8837608ce0db1d5de1bdd4fdda150354a00e
OYD03 <oumar.yacoubadiawara@gmail.com>
2022-12-01 11:12:29 +0100

    commit

--- oyd/vues/update/update_id.php
+++ oyd/vues/update/update_id.php
@@ -1,1 +1,32 @@
 <h2>Modifier l'identifiant de connexion</h2>
+<center>
+<form method="post">
+  <table>
+    <tr>
+      <td>
+        <label for="oldId">Identifiant actuel</label>
+      </td>
+      <td>
+        <input type="text" name="oldId" id="oldId">
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <label for="newId">Nouvel identifiant</label>
+      </td>
+      <td>
+        <input type="text" name="newId" id="newId">
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <label for="confNewId">Confirmer</label>
+      </td>
+      <td>
+        <input type="text" name="confNewId" id="confNewId">
+      </td>
+    </tr>
+  </table>
+  <input type="submit" name="Modifier" value="Modifier" class="sub-buton">
+</form>
+</center>
--- oyd/vues/update/update_mdp.php
+++ oyd/vues/update/update_mdp.php
@@ -1,1 +1,32 @@
 <h2>Modifier le mot de passe</h2>
+<center>
+<form method="post">
+  <table>
+    <tr>
+      <td>
+        <label for="oldMdp">Mot de passe Actuel</label>
+      </td>
+      <td>
+        <input type="text" name="oldMdp" id="oldMdp">
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <label for="newMdp">Nouvau mot de passe</label>
+      </td>
+      <td>
+        <input type="text" name="newMdp" id="newMdp">
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <label for="confNewMdp">Comfirmer</label>
+      </td>
+      <td>
+        <input type="text" name="confNewMdp" id="confNewMdp">
+      </td>
+    </tr>
+  </table>
+  <input type="submit" name="Modifier" value="Modifier" class="sub-buton">
+</form>
+</center>
