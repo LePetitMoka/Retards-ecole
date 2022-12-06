e15d826cde22b8d42b33c4d9636180c11cb697b4
OYD03 <oumar.yacoubadiawara@gmail.com>
2022-12-06 14:28:10 +0100

    show transports finis

--- oyd/vues/show/show_transports.php
+++ oyd/vues/show/show_transports.php
@@ -10,6 +10,7 @@
   $inpClass = '"invisible"';
   $classFull = '"full"';
   $classPartial = '"partial"';
+  $stateClass = '"state"';
   $totEl = count($lesTransports);
   $nbEp = 10;
   $totpg = floor($totEl/$nbEp);
@@ -27,31 +28,13 @@
       echo "<td class=".$classPartial.">".$unTransport['type']." ".$unTransport['nom']."</td>";
       echo "</tr>";
       echo "<tr>";
-      echo "<td class=".$classPartial."> Géré par : </td>";
-      echo "<td class=".$classPartial.">".$unTransport['transporteur']."</td>";
+      echo "<td class=".$classPartial."> Géré par : ".$unTransport['transporteur']."</td>";
       echo "</tr>";
       echo "<tr>";
       echo "<td class=".$classFull.">".$unTransport['type']."</td>";
-      echo "<td class=".$classPartial.">Etat : </td>";
-      echo "<td class=".$classPartial.">fluide</td>";
+      echo "<td class=".$stateClass.">Etat : fluide</td>";
       echo "</tr>";
       echo "</table>";
-      // echo "<div class='item-container'>";
-      // echo "<ul class='item-list unstyled-list item-list-left'>";
-      // echo "<li class='item-list-elements'><img src='".$unTransport['pictogramme']."' width='100' height='100'></li>";
-      // echo "<li class='item-list-elements'>".$unTransport['type']."</li>";
-      // echo "</ul>";
-      // echo "<ul class='item-list unstiled-list'>";
-      // echo "<li class='item-list-elements'>".$unTransport['type']."</li>";
-      // echo "<li class='item-list-elements'> Géré par : </li>";
-      // echo "<li class='item-list-elements'>Etat : </li>";
-      // echo "</ul>";
-      // echo "<ul class='item-list unstiled-list'>";
-      // echo "<li class='item-list-elements'>".$unTransport['nom']."</li>";
-      // echo "<li class='item-list-elements'>".$unTransport['transporteur']."</li>";
-      // echo "<li class='item-list-elements'>Ok</li>";
-      // echo "</ul>";
-      // echo "</div>";
     }
     // echo "</div>";
     echo "<form method='post'>";
@@ -71,16 +54,28 @@
   } else {
     $_SESSION['pg'] = 1;
     echo "<center>";
+    // echo "<div class='show-content'>";
     for($i=(($_SESSION['pg']-1)*$nbEp); $i<=($_SESSION['pg']*$nbEp-1); $i++){
       $unTransport = $lesTransports[$i];
-      echo $unTransport['IdTp']." ";
-      echo $unTransport['nom']." ";
-      echo $unTransport['type']."<br/>";
+      echo "<table class=".$tabClass.">";
+      echo "<tr>";
+      echo "<td rowspan='2' class=".$classFull."><img src='".$unTransport['pictogramme']."' width='100' height='100'></td>";
+      echo "<td class=".$classPartial.">".$unTransport['type']." ".$unTransport['nom']."</td>";
+      echo "</tr>";
+      echo "<tr>";
+      echo "<td class=".$classPartial."> Géré par : ".$unTransport['transporteur']."</td>";
+      echo "</tr>";
+      echo "<tr>";
+      echo "<td class=".$classFull.">".$unTransport['type']."</td>";
+      echo "<td class=".$stateClass.">Etat : fluide</td>";
+      echo "</tr>";
+      echo "</table>";
     }
+    // echo "</div>";
     echo "<form method='post'>";
     echo "<label for='pre' class=".$btnClass.">Precedente</label>";
     echo "<input type='submit' name='pre' value='pre' id='pre' class=".$inpClass.">";
-    echo "<label for='sui' class=".$btnClass.">suivant</a>";
+    echo "<label for='sui' class=".$btnClass.">suivant</label>";
     echo "<input type='submit' name='sui' value='sui' id='sui' class=".$inpClass.">";
     echo "</form>";
     echo "</center>";
