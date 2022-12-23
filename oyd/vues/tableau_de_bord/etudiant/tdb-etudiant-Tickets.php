2d0c71937efdf6fe38140654101cbd3ee0d6e931
OYD03 <oumar.yacoubadiawara@gmail.com>
2022-12-23 10:29:59 +0100

    correction vue billets etudiants

--- oyd/vues/tableau_de_bord/etudiant/tdb-etudiant-Tickets.php
+++ oyd/vues/tableau_de_bord/etudiant/tdb-etudiant-Tickets.php
@@ -3,8 +3,13 @@
 <section class="stats">
   <div class="container">
     <?php 
+      $lesInfos = array();
       $unControleur -> setTable("vue_totalbilleteleve");
       $lesInfos = $unControleur -> select_where("IdE", $_SESSION['id']);
+      if($lesInfos == false){
+        $lesInfos['nbBillets'] = 0;
+        $lesInfos['dureeCumulee'] = "00:00:00";
+      }
     ?>
     <p>Nombre total de retards : <?php echo $lesInfos['nbBillets'] ?></p>
     <p>Durée total de retards : <?php echo $lesInfos['dureeCumulee'] ?></p>
